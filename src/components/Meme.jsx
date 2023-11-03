a6e603eab4f51f5ebb464598c7e0c5d1b03574f8
WitchDevelops <d.n.wojewska@gmail.com>
2023-11-03 22:11:04 +0100

    add button click function

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,20 +1,37 @@
+import memesData from "../memesData";
 import "./Meme.css";
 
 const Meme = () => {
+    const memes = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memes.length);
+    let memeImage = memes[randomNumber].url;
+
+    const handleClick = () => { 
+        console.log(randomNumber);
+        console.log(memes[randomNumber].url);
+        memeImage = memes[randomNumber].url;
+    }
+
     return (
-        <main>
-            <form className="form">
+        <main className="main">
+            <p>{memeImage}</p>
+            <div className="form">
                 <div className="form__inputs">
                     <input type="text" className="form__input" placeholder="Top text"></input>
                     <input type="text" className="form__input" placeholder="Bottom text"></input>
                 </div>
 
-                <button type="submit" className="form__button">Get a new meme image
-                    <span class="material-symbols-outlined">
+                <button 
+                    type="submit" 
+                    className="form__button" 
+                    onClick={handleClick}>
+                        Get a new meme image
+                    <span className="material-symbols-outlined">
                         question_exchange
                     </span>
                 </button>
-            </form>
+                <img src={memeImage} className="meme__image"></img>
+            </div>
         </main>
     )
 }
