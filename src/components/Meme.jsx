c25db2dd5ef2bb125f822ed1e02d2f22da1072b4
Dominika Wojewska <112077394+WitchDevelops@users.noreply.github.com>
2023-11-15 14:53:49 +0000

    feat: change text when typing

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,8 +3,9 @@ import memesData from "../memesData";
 import "./Meme.css";
 
 const Meme = () => {
+    const [topText, setTopText] = useState();
     const [meme, setMeme] = useState({
-        topText: "",
+        topText: setTopText,
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg"
     });
@@ -19,12 +20,36 @@ const Meme = () => {
         }));
     }
 
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
     return (
         <main className="main">
             <div className="form">
                 <div className="form__inputs">
-                    <input type="text" className="form__input" placeholder="Top text"></input>
-                    <input type="text" className="form__input" placeholder="Bottom text"></input>
+                    <input 
+                        type="text" 
+                        className="form__input" 
+                        placeholder="Top text" 
+                        name="topText"
+                        maxLength={20} 
+                        value={meme.topText}
+                        onChange={handleChange}>
+                    </input>
+                    <input 
+                        type="text" 
+                        className="form__input" 
+                        placeholder="Bottom text" 
+                        name="bottomText"
+                        maxLength={20}
+                        value={meme.bottomText}
+                        onChange={handleChange}>
+                    </input>
                 </div>
 
                 <button
@@ -36,7 +61,12 @@ const Meme = () => {
                         question_exchange
                     </span>
                 </button>
-                <img src={meme.randomImage} className="meme__image"></img>
+                <div className="meme">
+                  <img src={meme.randomImage} className="meme__image"></img>
+                  <h2 className="meme__text--top">{meme.topText}</h2>
+                  <h2 className="meme__text--bottom">{meme.bottomText}</h2>  
+                </div>
+                
             </div>
         </main>
     )
