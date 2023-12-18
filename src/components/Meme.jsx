eff8d3d2b24338b2e3cb41bf1eadac42742bd0ae
WitchDevelops <d.n.wojewska@gmail.com>
2023-12-18 11:56:42 +0100

    #7 subst hardcoded labels with useId hook

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useId } from "react";
 import memesData from "../memesData";
 import "./Meme.css";
 
@@ -10,6 +10,8 @@ const Meme = () => {
         randomImage: "http://i.imgflip.com/1bij.jpg"
     });
     const [allMemeImages, setAllMemeImages] = useState(memesData);
+    const id = useId();
+
     const handleClick = () => {
         const memes = memesData.data.memes;
         const randomNumber = Math.floor(Math.random() * memes.length);
@@ -21,7 +23,7 @@ const Meme = () => {
     }
 
     const handleChange = (event) => {
-        const {name, value} = event.target;
+        const { name, value } = event.target;
         setMeme(prevMeme => ({
             ...prevMeme,
             [name]: value
@@ -33,34 +35,36 @@ const Meme = () => {
             <div className="form">
                 <div className="form__inputsCtn">
                     <div className="form__inputs">
-                       <label htmlFor="topText">
-                        Top text
-                    </label>
-                    <input 
-                        type="text"
-                        className="form__input" 
-                        placeholder="Top text" 
-                        name="topText"
-                        maxLength={20} 
-                        value={meme.topText}
-                        onChange={handleChange}>
-                    </input> 
+                        <label htmlFor={id + "-topText"}>
+                            Top text
+                        </label>
+                        <input
+                            type="text"
+                            className="form__input"
+                            placeholder="Top text"
+                            name="topText"
+                            id={id + "-topText"}
+                            maxLength={20}
+                            value={meme.topText}
+                            onChange={handleChange}>
+                        </input>
                     </div>
                     <div className="form__inputs">
-                        <label htmlFor="bottomText">
+                        <label htmlFor={id + "-bottomText"}>
                             Bottom text
                         </label>
-                        <input 
-                        type="text" 
-                        className="form__input" 
-                        placeholder="Bottom text" 
-                        name="bottomText"
-                        maxLength={20}
-                        value={meme.bottomText}
-                        onChange={handleChange}>
-                    </input>
+                        <input
+                            type="text"
+                            className="form__input"
+                            placeholder="Bottom text"
+                            name="bottomText"
+                            id = {id + "-bottomText"}
+                            maxLength={20}
+                            value={meme.bottomText}
+                            onChange={handleChange}>
+                        </input>
                     </div>
-                    
+
                 </div>
 
                 <button
@@ -73,11 +77,11 @@ const Meme = () => {
                     </span>
                 </button>
                 <div className="meme">
-                  <img src={meme.randomImage} className="meme__image"></img>
-                  <h2 className="meme__text--top">{meme.topText}</h2>
-                  <h2 className="meme__text--bottom">{meme.bottomText}</h2>  
+                    <img src={meme.randomImage} className="meme__image"></img>
+                    <h2 className="meme__text--top">{meme.topText}</h2>
+                    <h2 className="meme__text--bottom">{meme.bottomText}</h2>
                 </div>
-                
+
             </div>
         </main>
     )
